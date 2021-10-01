fd1a51c4f1cdcc23e14e7b20a0452141ec205435
Heonjun Jeong <gjswns1991@gmail.com>
2021-10-01 16:46:53 +0900

    Initial commit

---
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -0,0 +1,115 @@
+package com.example.study.repository;
+import com.example.study.StudyApplicationTests;
+import com.example.study.model.entity.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+//@DataJpaTest                                                                    // JPA 테스트 관련 컴포넌트만 Import
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)    // 실제 db 사용
+//@DisplayName("ItemRepositoryTest 테스트")
+public class UserRepositoryTest extends StudyApplicationTests {
+
+    // Dependency Injection (DI)
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void create(){
+        String account = "Test03";
+        String password = "Test03";
+        String status = "REGISTERED";
+        String email = "Test01@gmail.com";
+        String phoneNumber = "010-1111-3333";
+        LocalDateTime registeredAt = LocalDateTime.now();
+        LocalDateTime createdAt = LocalDateTime.now();
+        String createdBy = "AdminServer";
+
+
+        User user = new User();
+        user.setAccount(account);
+        user.setPassword(password);
+        user.setStatus(status);
+        user.setEmail(email);
+        user.setPhoneNumber(phoneNumber);
+        user.setRegisteredAt(registeredAt);
+
+        // 빌더 패턴으로 객체 생성
+       // User u = User.builder().account(account).password(password).status(status).email(email).build();
+        // 체인 패턴으로 객체 업데이트
+        // u.setEmail("").setPhoneNumber("");
+        User newUser = userRepository.save(user);
+        Assertions.assertNotNull(newUser);
+    }
+
+    @Test
+    @Transactional
+    public void read(){
+
+        User user = userRepository.findFirstByPhoneNumberOrderByIdDesc("010-1111-2222");
+
+
+        if(user!=null) {
+            user.getOrderGroupList().forEach(orderGroup -> {
+                System.out.println("-------------주문묶음---------------");
+                System.out.println("수령인 : " + orderGroup.getRevName());
+                System.out.println("수령지 : " + orderGroup.getRevAddress());
+                System.out.println("총금액 : " + orderGroup.getTotalPrice());
+                System.out.println("총수량 : " + orderGroup.getTotalQuantity());
+
+                System.out.println("-------------주문상세---------------");
+
+                orderGroup.getOrderDetailList().forEach(orderDetail -> {
+                    System.out.println("파트너사 이름 : "+orderDetail.getItem().getPartner().getName());
+                    System.out.println("파트너사 카테고리 : "+orderDetail.getItem().getPartner().getCategory().getTitle());
+                    System.out.println("주문 상품 : "+orderDetail.getItem().getName());
+                    System.out.println("고객센터 번호 : " +orderDetail.getItem().getPartner().getCallCenter());
+                    System.out.println("주문의 상태 : "+orderDetail.getStatus());
+                    System.out.println("도착예정일자 : "+orderDetail.getArrivalDate());
+                });
+            });
+        }
+
+        Assertions.assertNotNull(user);
+    }
+
+    @Test
+    @Transactional
+    public void update(){
+
+        Optional<User> user = userRepository.findById(2L);
+
+        user.ifPresent(selectUser ->{
+            selectUser.setAccount("PPPP");
+            selectUser.setUpdatedAt(LocalDateTime.now());
+            selectUser.setUpdatedBy("update method()");
+
+            userRepository.save(selectUser);
+        });
+    }
+
+    @Test
+    @Transactional
+    public void delete(){
+        Optional<User> user = userRepository.findById(6L);
+
+        Assertions.assertTrue(user.isPresent());    // false
+
+
+        user.ifPresent(selectUser->{
+            userRepository.delete(selectUser);
+        });
+
+        Optional<User> deleteUser = userRepository.findById(3L);
+
+        Assertions.assertFalse(deleteUser.isPresent()); // false
+    }
+
+}
